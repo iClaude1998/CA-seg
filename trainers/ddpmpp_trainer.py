9665a7100ac27b2467a40f78313199d10aa75d16
yunxiang.liu <yunxiang.liu@aiml.team>
2024-11-05 13:23:18 +1030

    pull request

--- trainers/ddpmpp_trainer.py
+++ trainers/ddpmpp_trainer.py
@@ -225,7 +225,7 @@ class DDPMPP_Trainer(object):
                     self.writer.add_scalar('MSE', mean_loss_dict['mse'], iter_id)
                     self.writer.add_scalar('Training Loss', mean_loss_dict["vb"], iter_id)
             
-            if iter_id % (self.save_interval * 100) == 0:
+            if iter_id % (self.save_interval * 500) == 0:
                 vts, random_batch = self.random_inference()
                 if self.accelerator.is_local_main_process:
                     self.visualize(vts, random_batch, iter_id)
