9b4dee52a470bb117d045b91a514d0e1654c4a02
yunxiang.liu <yunxiang.liu@aiml.team>
2025-02-10 16:00:55 +1030

    cancel the usdf

--- datasets/segmentations/bioparse_multiclss.py
+++ datasets/segmentations/bioparse_multiclss.py
@@ -64,15 +64,9 @@ class Bioparse_segmentation(Dataset):
         self.train_rate = train_rate
         self.img_dir = os.path.join(root_dir, modality, f'{split}')
         self.mask_dir = os.path.join(root_dir, modality, f"{split}_mask")
-<<<<<<< HEAD
-        self.inter_dir = os.path.join(root_dir, modality, f"{split}_cbm", self.organ)
-        # self.sdf_dir = zarr.open(os.path.join(root_dir, modality, f"{split}_usdf"), mode='r')
-        self.preprocess, _, image_resolution = preprocessors
-=======
         self.inter_dir = os.path.join(root_dir, modality, f"{split}_cbm")
         self.sdf_dir = zarr.open(os.path.join(root_dir, modality, f"{split}_usdf"), mode='r')
         self.preprocess, self.tokenizer, image_resolution = preprocessors
->>>>>>> 96e7e85ee70298390c312f5553adbe46812e76d8
 
         if image_size is not None and image_size != image_resolution:
             image_resolution = image_size
