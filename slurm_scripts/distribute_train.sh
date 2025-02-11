e491a845479ddf25026ab649710a99ac2dc6cb12
iClaude1998 <1316761893@qq.com>
2025-02-12 01:25:00 +1030

    pull

--- slurm_scripts/distribute_train.sh
+++ slurm_scripts/distribute_train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Configure the resources required
-#SBATCH --job-name=yliusb-rlpclip22 # job name
+#SBATCH --job-name=zhezhongren # job name
 #SBATCH -p a100
 #SBATCH -N 1 # number of tasks (sequential job starts 1 task) (check this if your job unexpectedly uses 2 nodes)
 #SBATCH --ntasks=2          # number of tasks (multi-thread job starts 2 tasks)
@@ -8,6 +8,7 @@
 #SBATCH -c 8                # number of cores (sequential job calls a multi-thread program that uses 8 cores)
 #SBATCH --time=18:00:00         # time allocation, which has the format (D-HH:MM), here set to 1 hour
 #SBATCH --gres=gpu:2            # generic resource required (here requires 2 GPUs)
+#SBATCH --chdir=/gpfs/users/a1233646/myprojects/clipflow2 # set the working directory
 
 # Configure notifications
 #SBATCH --mail-type=END
